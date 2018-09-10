40e7128356b7d492f24bac7abb281496fd2bbe4a
Florian Bougel <florianbougel@gmail.com>
2018-09-10 11:10:33 +0200

    Use Rest and Spread Operators

--- es6.js
+++ es6.js
@@ -2,21 +2,30 @@ const offCanvas  = document.querySelector('.c-off-canvas'),
       menuToggle = document.querySelector('.c-menu__toggle'),
       main       = document.querySelector('.o-main');
 
-const closeMenu = [menuToggle, main];
+const closingElements = [menuToggle, main];
+const toggleElements  = [offCanvas, main];
 
 menuToggle.addEventListener('click', e => {
   e.preventDefault();
   e.stopImmediatePropagation();
-  offCanvas.classList.toggle('is-open');
-  main.classList.toggle('is-open');
+  openElements(...toggleElements);
 });
 
-for (let i = 0; i < closeMenu.length; i++) {
-  closeMenu[i].addEventListener('click', e => {
+closingElements.forEach(closingElem => {
+  closingElem.addEventListener('click', e => {
     e.preventDefault();
     if (main.classList.contains('is-open') && !(e.target).classList.contains('menu-toggle')) {
-      main.classList.remove('is-open');
-      offCanvas.classList.remove('is-open');
+      closeElements(...toggleElements);
     }
-  });
+  })
+});
+
+function openElements(...elements) {
+  const elementsToOpen = [...elements];
+  elementsToOpen.forEach(elementToOpen => elementToOpen.classList.toggle('is-open'));
+}
+
+function closeElements(...elements) {
+  const elementsToClose = [...elements];
+  elementsToClose.forEach(elementToClose => elementToClose.classList.remove('is-open'));
 }
